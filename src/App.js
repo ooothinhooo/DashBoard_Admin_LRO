59ad2cdd3d6800f32ab48e8fb4f95953a28535eb
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2023-03-02 14:17:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import { Layout } from "./components/index.js";
-
+import "./App.css";
+import { useEffect, useState, StrictMode, useContext } from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <>
-      <Layout />
+      {/* <Layout /> */}
+      <StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<Layout />} />
+          </Routes>
+        </BrowserRouter>
+      </StrictMode>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { StrictMode } from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const rootElement = document.getElementById("root");
+render(
+  <>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </>,
+  rootElement
 );
-
-
