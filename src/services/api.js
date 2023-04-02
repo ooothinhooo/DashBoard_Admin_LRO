8c018e162fed580ccc4992cd3c540930e8923a21
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2023-04-02 22:34:59 +0700

    up

--- src/services/api.js
+++ src/services/api.js
@@ -11,7 +11,7 @@ import {
   StatisticsDocs,
   StatisticsUsers,
 } from "./apiConstan.js";
-// const API_URL = "http://localhost:8080";
+// const API_URL = "https://api-lro.onrender.com";
 export const API_URL = process.env.REACT_APP_API_KEY;
 
 export class api {
